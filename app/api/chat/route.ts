e3cb6598b6314ad4bd047d235cd2e8102d4f2be6
sh970901 <aaddss639@naver.com>
2024-04-14 02:36:15 +0900

    etc: bedrock-chat model로 변경 및 테스트, vercel ai, langchain을 활용한 챗 모델 구현

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,73 +1,105 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
+// import {
+//   BedrockRuntimeClient,
+//   InvokeModelWithResponseStreamCommand,
+// } from '@aws-sdk/client-bedrock-runtime';
+// import { AWSBedrockAnthropicStream, StreamingTextResponse } from 'ai';
+// import { experimental_buildAnthropicPrompt } from 'ai/prompts';
 
-import { ChatOpenAI } from "@langchain/openai";
+// // IMPORTANT! Set the runtime to edge
+// export const runtime = 'edge';
+ 
+// const bedrockClient = new BedrockRuntimeClient({
+//   region: process.env.AWS_REGION ?? 'us-east-1',
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+//   },
+// });
+ 
+// export async function POST(req: Request) {
+
+//   // Extract the `prompt` from the body of the request
+//   const { messages } = await req.json();
+
+
+ 
+//   // // Ask Claude for a streaming chat completion given the prompt
+//   const bedrockResponse = await bedrockClient.send(
+//     new InvokeModelWithResponseStreamCommand({
+//       modelId: 'anthropic.claude-v2',
+//       contentType: 'application/json',
+//       accept: 'application/json',
+//       body: JSON.stringify({
+//         prompt: experimental_buildAnthropicPrompt(messages),
+//         max_tokens_to_sample: 300,
+//       }),
+//     }),
+//   );
+
+ 
+//   // Convert the response into a friendly text-stream
+//   const stream = AWSBedrockAnthropicStream(bedrockResponse);
+ 
+//   // Respond with the stream
+//   return new StreamingTextResponse(stream);
+// }
+
+
+import { StreamingTextResponse } from 'ai';
+import { Message as VercelChatMessage } from "ai";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { HttpResponseOutputParser } from "langchain/output_parsers";
+import { BedrockChat } from "@langchain/community/chat_models/bedrock/web";
 
-export const runtime = "edge";
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
 
 const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
 
-const TEMPLATE = `You are a pirate named Patchy. All responses must be extremely verbose and in pirate dialect.
+const TEMPLATE = `You are a muliple developer.
 
 Current conversation:
 {chat_history}
 
 User: {input}
 AI:`;
+ 
+export async function POST(req: Request) {
 
-/**
- * This handler initializes and calls a simple chain with a prompt,
- * chat model, and output parser. See the docs for more information:
- *
- * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
- */
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    const messages = body.messages ?? [];
-    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
-    const currentMessageContent = messages[messages.length - 1].content;
-    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+  // Extract the `prompt` from the body of the request
+  // const { messages } = await req.json();
 
-    /**
-     * You can also try e.g.:
-     *
-     * import { ChatAnthropic } from "langchain/chat_models/anthropic";
-     * const model = new ChatAnthropic({});
-     *
-     * See a full list of supported models at:
-     * https://js.langchain.com/docs/modules/model_io/models/
-     */
-    const model = new ChatOpenAI({
-      temperature: 0.8,
-      modelName: "gpt-3.5-turbo-1106",
-    });
+  const body = await req.json();
+  const messages = body.messages ?? [];
+  const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+  const currentMessageContent = messages[messages.length - 1].content;
+  const prompt = PromptTemplate.fromTemplate(TEMPLATE);
 
-    /**
-     * Chat models stream message chunks rather than bytes, so this
-     * output parser handles serialization and byte-encoding.
-     */
-    const outputParser = new HttpResponseOutputParser();
+  const model = new BedrockChat({
+    model: "anthropic.claude-v2",
+  region: process.env.AWS_REGION ?? 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+  },
+  });
+    
+  // const res = await model.invoke("Tell me a joke");
+  // console.log(res);
 
-    /**
-     * Can also initialize as:
-     *
-     * import { RunnableSequence } from "@langchain/core/runnables";
-     * const chain = RunnableSequence.from([prompt, model, outputParser]);
-     */
-    const chain = prompt.pipe(model).pipe(outputParser);
-
-    const stream = await chain.stream({
-      chat_history: formattedPreviousMessages.join("\n"),
-      input: currentMessageContent,
-    });
-
-    return new StreamingTextResponse(stream);
-  } catch (e: any) {
-    return NextResponse.json({ error: e.message }, { status: e.status ?? 500 });
-  }
+  const outputParser = new HttpResponseOutputParser();
+  const chain = prompt.pipe(model).pipe(outputParser);
+  const stream = await chain.stream({
+    chat_history: formattedPreviousMessages.join("\n"),
+    input: currentMessageContent,
+  });
+ 
+  // Convert the response into a friendly text-stream
+  // const stream = AWSBedrockAnthropicStream(bedrockResponse);
+ 
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
 }
